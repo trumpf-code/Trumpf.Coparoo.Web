caccf668da8d2b3b983b169b9e586c377e998c8a
Phi Dang <Phi.Dang@axoom.com>
2018-02-02 16:25:23 +0100

    Added button control object

---
+++ Trumpf.Coparoo.Web/Controls/Button.cs
@@ -0,0 +1,20 @@
+﻿namespace Trumpf.Coparoo.Web.Controls
+{
+    using OpenQA.Selenium;
+
+    /// <summary>
+    /// Button control object.
+    /// </summary>
+    public class Button : ControlObject
+    {
+        /// <summary>
+        /// Gets the search pattern.
+        /// </summary>
+        protected override By SearchPattern => By.TagName("button");
+
+        /// <summary>
+        /// Gets the button text.
+        /// </summary>
+        public string Text => Node.Text;
+    }
+}
