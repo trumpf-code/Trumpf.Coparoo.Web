d9e10d8c591dc60642dc1837669098f0fe78f9cc
Phi Dang <Phi.Dang@axoom.com>
2018-04-27 13:51:59 +0200

    Added checkbox control object

---
+++ Trumpf.Coparoo.Web/Controls/Checkbox.cs
@@ -0,0 +1,34 @@
+﻿namespace Trumpf.Coparoo.Web.Controls
+{
+    using OpenQA.Selenium;
+    using OpenQA.Selenium.Interactions;
+
+    /// <summary>
+    /// Checkbox control object.
+    /// Expects an input html element with attribute type="checkbox".
+    /// </summary>
+    public class Checkbox : ControlObject
+    {
+        /// <summary>
+        /// Gets the search pattern.
+        /// </summary>
+        protected override By SearchPattern => By.XPath(".//input[@type='checkbox']");
+
+        /// <summary>
+        /// Gets or sets the checked attribute.
+        /// </summary>
+        public bool Checked
+        {
+            get { return Node.GetAttribute("checked") != null; }
+
+            set
+            {
+                if (Checked != value)
+                {
+                    var element = Parent.Node.FindElement(SearchPattern);
+                    new Actions(Root.Driver).MoveToElement(element).Click().Perform();
+                }
+            }
+        }
+    }
+}
