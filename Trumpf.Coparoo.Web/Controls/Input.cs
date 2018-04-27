61d8ca552926d8a296424d16d63f295ad7f62d32
Phi Dang <Phi.Dang@axoom.com>
2018-04-27 13:53:05 +0200

    Added input text control object

---
+++ Trumpf.Coparoo.Web/Controls/Input.cs
@@ -0,0 +1,37 @@
+﻿namespace Trumpf.Coparoo.Web.Controls
+{
+    using OpenQA.Selenium;
+
+    /// <summary>
+    /// Text input control object.
+    /// Expects an input html element with attribute type="text".
+    /// </summary>
+    public class Input : ControlObject
+    {
+        /// <summary>
+        /// Gets the search pattern.
+        /// </summary>
+        protected override By SearchPattern => By.XPath(".//input[@type='text']");
+
+        /// <summary>
+        /// Gets or sets the text content.
+        /// </summary>
+        public string Text
+        {
+            get { return Node.GetAttribute("value"); }
+
+            set
+            {
+                if (Text != value)
+                {
+                    if (Text != string.Empty)
+                    {
+                        Node.Clear();
+                    }
+
+                    Node.SendKeys(value);
+                }
+            }
+        }
+    }
+}
