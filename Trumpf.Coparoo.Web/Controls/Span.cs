41855424cc26679c46af97e770c4aa47ebea4f33
Phi Dang <Phi.Dang@axoom.com>
2018-04-27 13:52:22 +0200

    Added div, label and span control object

---
+++ Trumpf.Coparoo.Web/Controls/Span.cs
@@ -0,0 +1,20 @@
+﻿namespace Trumpf.Coparoo.Web.Controls
+{
+    using OpenQA.Selenium;
+
+    /// <summary>
+    /// Span control object.
+    /// </summary>
+    public class Span : ControlObject
+    {
+        /// <summary>
+        /// Gets the search pattern.
+        /// </summary>
+        protected override By SearchPattern => By.TagName("span");
+
+        /// <summary>
+        /// Gets the text.
+        /// </summary>
+        public string Text => Node.Text;
+    }
+}
